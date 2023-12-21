fdd4390dfac3f1458592088854f42eea19a4aba5
LeandroL18 <leandrolipani6@gmail.com>
2023-12-21 16:01:56 +0100

    feat: structure default

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{
+        name:'AppHeader'
+    }
+</script>
+
+<template>
+    <h1>Ciao!</h1>
+</template>
+
+<style lang="scss" scoped>
+    h1{color: black;}
+</style>

378f54ca6cd63bf5d7e73db4843b9b12f9a3227d
LeandroL18 <leandrolipani6@gmail.com>
2024-01-08 14:30:52 +0100

    feat: AppHeader, ListProducts

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,54 @@
 </script>
 
 <template>
-    <h1>Ciao!</h1>
+    <header>
+        <!-- logo -->
+        <div>
+            <img src="../img/dc-logo.png" alt="logo">
+        </div>
+        <!-- nav_bar -->
+        <nav>
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a class="active" href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <style lang="scss" scoped>
-    h1{color: black;}
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    header{
+        display:flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 20px;
+    }
+    img{
+        width: 70%;
+    }
+    nav ul li{
+        display: inline-block;
+        margin: 0 8px 10px 8px;
+        font-size: 12px;
+    }
+    li a{
+        color: rgb(0, 0, 0);
+        font-weight: 800;
+
+        &.active, &:hover{
+            color: blue;
+        }
+
+    }
 </style>
---
+++ src/components/ListProducts.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        name:'ListProducts'
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Content goes here</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    div{
+        background-color: black;
+        padding: 30px 0;
+    }
+    h1{
+        color: rgb(255, 255, 255);
+        text-align: center;
+    }
+    
+</style>
